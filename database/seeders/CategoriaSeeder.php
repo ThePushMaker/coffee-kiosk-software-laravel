d6829cdc7750cf90f024db91e83318b4b8388c01
Martin Calderón <elmcwl@gmail.com>
2024-03-28 20:47:34 -0700

    feat: adding a seeder

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Café',
+            'icono' => 'cafe',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Hamburguesas',
+            'icono' => 'hamburguesa',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Pizzas',
+            'icono' => 'pizza',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Donas',
+            'icono' => 'dona',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Pasteles',
+            'icono' => 'pastel',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Galletas',
+            'icono' => 'galletas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
