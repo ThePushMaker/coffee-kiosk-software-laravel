288bb3b5c1133ec4f17ed3eecfe2952352c69fd8
Martin Calderón <elmcwl@gmail.com>
2024-03-31 08:36:46 -0700

    feat: added collection of products, pagination and more

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductoCollection;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        //
+        return new ProductoCollection(Producto::where('disponible', 1)->orderBy('id', 'DESC')->paginate(10));
     }
 
     /**
--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/categorias', CategoriaController::class);
+Route::apiResource('/productos', ProductoController::class);
