58bc6b33ddc5a138b126d4a39d5b092f74276046
Martin Calderón <elmcwl@gmail.com>
2024-03-31 18:55:24 -0700

    feat: created an authentication request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistroRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegistroRequest $request)
     {
-        
+        // validar el registro
+        $data = $request->validated();
     }
     
     public function login(Request $request)
