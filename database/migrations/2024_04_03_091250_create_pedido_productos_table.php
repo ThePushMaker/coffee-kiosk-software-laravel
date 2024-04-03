e1d43a58b539d75a19b0e5b93a41634c5ba97bc7
Martin Calderón <elmcwl@gmail.com>
2024-04-03 02:17:52 -0700

    feat: creating productsOrders table

---
+++ database/migrations/2024_04_03_091250_create_pedido_productos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedido_productos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('producto_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedido_productos');
+    }
+};
