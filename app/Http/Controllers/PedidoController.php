04864cebfe409763ae878eca29ab723e80ac15a4
Martin Calderón <elmcwl@gmail.com>
2024-04-03 03:03:51 -0700

    feat: saving the order products in the DB

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pedido;
+use App\Models\PedidoProducto;
+use App\Models\Producto;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,8 +30,31 @@ class PedidoController extends Controller
         $pedido->total = $request->total;
         $pedido->save();
         
+        // obtener el ID del pedido
+        $id = $pedido->id;
+        
+        // obtener los productos
+        $productos = $request->productos;
+        
+        // formatear un arreglo
+        $pedido_producto = [];
+        
+        foreach($productos as $producto) {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ];
+        }
+        
+        // almacenar en la DB
+        PedidoProducto::insert($pedido_producto);
+        
         return [
-            'message' => 'realizando pedido'
+            'message' => 'realizando pedido' . $pedido->id,
+            'productos' => $request->productos
         ];
     }
 
