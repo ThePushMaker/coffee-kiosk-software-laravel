0fe3f4ee015d2c3ca30430046b26d323ce55b977
Martin Calderón <elmcwl@gmail.com>
2024-04-03 19:31:32 -0700

    feat: adding the user who performs the action within the JSON

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PedidoCollection;
 use App\Models\Pedido;
 use App\Models\PedidoProducto;
 use App\Models\Producto;
@@ -16,7 +17,7 @@ class PedidoController extends Controller
      */
     public function index()
     {
-        return "correcto";
+        return new PedidoCollection(Pedido::with('user')->where('estado', 0)->get());
     }
 
     /**
--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
-{
+{ 
     use HasFactory;
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
